df0e90f82cb58c8f032041b010f30d437f180782
ReginaldoJLima <reginaldojlima963@gmail.com>
2020-11-12 18:06:27 -0300

    Botão área administrativa da navegação

--- app/site/paginas/includes/navegacao.php
+++ app/site/paginas/includes/navegacao.php
@@ -15,16 +15,37 @@
           <li class="nav-item active"><a class="nav-link <?php echo (($_GET['pg'] == 'contato') ? 'btn py-2 btn-outline-primary' : ''); ?>"" href=" ?pg=contato">Contato</a></li>
         </ul>
 
-        <ul class="navbar-nav">
-          <li class="nav-item dropdown">
-            <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-              <span class="regi-user"></span>
-            </a>
-            <div class="dropdown-menu" aria-labelledby="navbarDropdown">
-              <a class="dropdown-item text-danger" href="#"><span class="regi-exit"></span> Sair</a>
-            </div>
-          </li>
-        </ul>
+        <?php
+
+        if (isset($_SESSION[''])) { ?>
+          <ul class="navbar-nav">
+            <li class="nav-item dropdown">
+              <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                <span class="regi-user"></span> Login
+              </a>
+              <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+                <a class="dropdown-item text-danger" href="#"><span class="regi-exit"></span> Sair</a>
+              </div>
+            </li>
+          </ul>
+        <?php
+        } else { ?>
+          <ul class="navbar-nav">
+            <li class="nav-item active">
+              <a class="nav-link btn btn-outline-success" href="cpanel.php?pg=cpanel">Administração do Site</a>
+            </li>
+          </ul>
+        <?php
+        }
+        ?>
+
+
+
+
+
+
+
+
       </div>
     </nav>
   </div>
