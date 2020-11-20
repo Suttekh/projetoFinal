cecb725965e58f45aacb025d6698d6f8523d56a6
ReginaldoJLima <reginaldojlima963@gmail.com>
2020-11-20 13:31:41 -0300

    Antes do Banco de dados

--- app/painelAdm/paginas/includes/header.php
+++ app/painelAdm/paginas/includes/header.php
@@ -13,25 +13,28 @@
   <!-- Ionicons -->
   <link rel="stylesheet" href="https://code.ionicframework.com/ionicons/2.0.1/css/ionicons.min.css">
   <!-- Tempusdominus Bootstrap 4 -->
-  <link rel="stylesheet" href="app/paineladm/plugins/tempusdominus-bootstrap-4/css/tempusdominus-bootstrap-4.min.css">
+  <link rel="stylesheet" href="app/painelAdm/plugins/tempusdominus-bootstrap-4/css/tempusdominus-bootstrap-4.min.css">
   <!-- iCheck -->
-  <link rel="stylesheet" href="app/paineladm/plugins/icheck-bootstrap/icheck-bootstrap.min.css">
+  <link rel="stylesheet" href="app/painelAdm/plugins/icheck-bootstrap/icheck-bootstrap.min.css">
   <!-- JQVMap -->
-  <link rel="stylesheet" href="app/paineladm/plugins/jqvmap/jqvmap.min.css">
+  <link rel="stylesheet" href="app/painelAdm/plugins/jqvmap/jqvmap.min.css">
 
   <!-- DataTables -->
-  <link rel="stylesheet" href="app/paineladm/plugins/datatables-bs4/css/dataTables.bootstrap4.min.css">
-  <link rel="stylesheet" href="app/paineladm/plugins/datatables-responsive/css/responsive.bootstrap4.min.css">
-  <link rel="stylesheet" href="app/paineladm/plugins/datatables-buttons/css/buttons.bootstrap4.min.css">
+  <link rel="stylesheet" href="app/painelAdm/plugins/datatables-bs4/css/dataTables.bootstrap4.min.css">
+  <link rel="stylesheet" href="app/painelAdm/plugins/datatables-responsive/css/responsive.bootstrap4.min.css">
+  <link rel="stylesheet" href="app/painelAdm/plugins/datatables-buttons/css/buttons.bootstrap4.min.css">
+
+  <!-- Icomoon -->
+  <link rel="stylesheet" href="app/painelAdm/plugins/icomoon/style.css">
 
   <!-- Theme style -->
-  <link rel="stylesheet" href="app/paineladm/dist/css/adminlte.min.css">
+  <link rel="stylesheet" href="app/painelAdm/dist/css/adminlte.min.css">
   <!-- overlayScrollbars -->
-  <link rel="stylesheet" href="app/paineladm/plugins/overlayScrollbars/css/OverlayScrollbars.min.css">
+  <link rel="stylesheet" href="app/painelAdm/plugins/overlayScrollbars/css/OverlayScrollbars.min.css">
   <!-- Daterange picker -->
-  <link rel="stylesheet" href="app/paineladm/plugins/daterangepicker/daterangepicker.css">
+  <link rel="stylesheet" href="app/painelAdm/plugins/daterangepicker/daterangepicker.css">
   <!-- summernote -->
-  <link rel="stylesheet" href="app/paineladm/plugins/summernote/summernote-bs4.min.css">
+  <link rel="stylesheet" href="app/painelAdm/plugins/summernote/summernote-bs4.min.css">
 </head>
 
 <body class="hold-transition sidebar-mini layout-fixed">
--- app/painelAdm/paginas/produtos.php
+++ app/painelAdm/paginas/produtos.php
@@ -1,4 +1,6 @@
-    <!-- Content Wrapper. Contains page content -->
+
+
+<!-- Content Wrapper. Contains page content -->
     <div class="content-wrapper">
         <!-- Content Header (Page header) -->
         <div class="content-header">
